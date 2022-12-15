a8806f08311317a4fc7045d4cc8db6128681d78f
Christina Papastolopoulou <papastolopoulou@gmail.com>
2022-12-15 11:02:16 +0100

    fix test files

---
+++ data/Untitled.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import pytest
+import os
+import temperature_plotting as tpl
+def test_compute_mean():
+    calc = tpl.compute_mean([0,10,20])
+    assert calc == 10
+    assert type(calc) == float
+    
+    calc = tpl.compute_mean([-10, 10])
+    assert calc == 0
+    
+    calc = tpl.compute_mean([0,10,0]) #define how many degits otherwise it will complain
+    assert round(calc,4) == 3.3333 , "Check that average is correct"
+    
+    with pytest.raises(TypeError, "expected type error"):
+        calc = tpl.compute_mean(["a", "b", "c"])  #it will go wrong
+
+    calc = pytest.compute_mean([]) # Not recommended
+    assert calc == None
+    
+test_compute_mean()
+
+def test_main():
+    tlp.main()
+    assert os.path.exists("plot_25.png")
+
+test_main()
+
